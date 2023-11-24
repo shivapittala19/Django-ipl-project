a8fa57a4ac8e506e5e5b8b1561c0c3dcbc968235
shivapittala19 <shivapittala2001@gmail.com>
2023-11-24 12:16:48 +0530

    intial commit

--- ipl_app/management/commands/import_dataset.py
+++ ipl_app/management/commands/import_dataset.py
@@ -17,17 +17,17 @@ class Command(BaseCommand):
             self.stdout.write(self.style.ERROR(f'Error during data import: {e}'))
 
     def import_matches(self):
-        # self.stdout.write(self.style.SUCCESS('Importing matches...'))
+        self.stdout.write(self.style.SUCCESS('Importing matches...'))
 
         with open('matches.csv', 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 Matches.objects.create(**row)
                 
-        # self.stdout.write(self.style.SUCCESS('Matches imported successfully.'))
+        self.stdout.write(self.style.SUCCESS('Matches imported successfully.'))
 
     def import_deliveries(self):
-        # self.stdout.write(self.style.SUCCESS('Importing deliveries...'))
+        self.stdout.write(self.style.SUCCESS('Importing deliveries...'))
 
         with open('deliveries.csv', 'r') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -35,4 +35,4 @@ class Command(BaseCommand):
                 match_id_id = row.pop('match_id')
                 match_id = Matches.objects.get(id=match_id_id)
                 Deliveries.objects.create(match_id=match_id, **row)
-        # self.stdout.write(self.style.SUCCESS('Deliveries imported successfully.'))
+        self.stdout.write(self.style.SUCCESS('Deliveries imported successfully.'))
